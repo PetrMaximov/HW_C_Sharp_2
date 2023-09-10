b9ff5813eccf5cf2c77e36b625255e6086975b23
«Petr» <maxpetrb@mail.ru>
2023-09-10 16:03:46 +0300

    Task 10 Вторая цифра трехзначного числа

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+Console.WriteLine("Введите трехзначное число");
+
+int Num = Convert.ToInt32(Console.ReadLine());
+
+int Num100 = Num / 100;
+int Num10 = (Num - Num100*100) / 10;
+int Num1 = Num % 10;
+
+Console.WriteLine("вторая цифра числа " + Num + ": " + Num10);
+
