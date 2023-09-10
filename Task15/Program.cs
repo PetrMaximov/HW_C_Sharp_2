a365993f5f1213b9333f57770a14c17574f17d83
«Petr» <maxpetrb@mail.ru>
2023-09-10 17:58:06 +0300

    Task15 день недели является выходным?

---
+++ Task15/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите номер дня недели");
+int NumDay = Convert.ToInt32(Console.ReadLine());
+
+if ((0 < NumDay) && (NumDay < 6)) Console.WriteLine("Нет");
+if ((5 < NumDay) && (NumDay < 8)) Console.WriteLine("Да");
+if (NumDay < 1) Console.WriteLine("Введен неверный номер");
+if (NumDay > 7) Console.WriteLine("Введен неверный номер");
